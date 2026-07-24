bad53285dbae266aa99c58a2cb249487557df93f
kdkrkwhr <kdkdongki1997@gmail.com>
2026-07-25 05:48:31 +0900

    stock: 다크/라이트 테마 토글 (헤더 🌙/☀️, sw v8)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ponytail: network-first data, cache shell for offline */
-const CACHE = "stock-challenge-v7";
+const CACHE = "stock-challenge-v8";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
