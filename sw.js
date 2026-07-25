530ef11ee6d4bff506deb26daf7a1cb1736573b4
kdkrkwhr <kdkdongki1997@gmail.com>
2026-07-25 14:19:54 +0900

    stock: 헤더에 시세 수동 새로고침 버튼 추가 (🔄)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ponytail: network-first data, cache shell for offline */
-const CACHE = "stock-challenge-v11";
+const CACHE = "stock-challenge-v12";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
